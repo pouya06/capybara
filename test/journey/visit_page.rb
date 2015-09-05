e06a8704fd90486ae5e761939307a0c42a192490
Pouya <pouya06@gmail.com>
2015-09-04 22:14:06 -0700

    adding project to github/capybara

--- test/journey/visit_page.rb
+++ test/journey/visit_page.rb
@@ -3,9 +3,14 @@ require_relative 'helper/spec_helper'
 
 extend ::RSpec::Matchers
 
-describe 'Login Page', :type => :feature do
-  it 'should go to page' do
+describe 'Login to facebook', :type => :feature do
+  it 'should be able to log in' do
+    visit ('https://www.facebook.com')
+    fill_in('email', :with => 'pouya_2002@yahoo.com')
+    fill_in('pass', :with => 'karimi1984')
+    click_button 'Log In'
+    page.find('a[herf="https://www.facebook.com/babak.alipour.3""]').click
     sleep 25
-   # expect(page).to have_content 'google'
+    #expect(page).to have_content 'google'
   end
 end
